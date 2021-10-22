3309983aba749f625f863c4a0105892c8cc18a10
IvoOjvan <ivos.ojvan@gmail.com>
2021-10-22 10:16:54 +0200

    Damage calculator
    Calculation damage taken depending on % resistance  of a player.

---
+++ DZ1/AV1-Zad_4.2/AV1-Zadatak_4.2/AV1-Zadatak_4.2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AV1_Zadatak_4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Damage damage = new Damage(10, 10, 20);
+            Resistance resistance = new Resistance();
+            Player player = new Player("Pero", damage, resistance);
+            player.ShowStats();
+            Console.WriteLine($"Damge amount: {damage.DamageAmount()}");
+            Console.WriteLine($"Damage taken: {player.TakeDamage()}");
+        }
+    }
+}
---
+++ DZ1/AV1-Zad_4.2/AV1-Zadatak_4.2/AV1-Zadatak_4.2/Resistance.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AV1_Zadatak_4._2
+{
+    class Resistance
+    {
+        #region Properties
+        private double physicalDamageResistance;
+        public double PhysicalDamageResistance
+        {
+            get { return this.physicalDamageResistance; }
+            set { this.physicalDamageResistance = value; }
+        }
+
+        private double magicDamageResistance;
+        public double MagicDamageResistance
+        {
+            get { return this.magicDamageResistance; }
+            set { this.magicDamageResistance = value; }
+        }
+        #endregion
+
+        #region Constructor
+        public Resistance()
+        {
+            Random random = new Random();
+            MagicDamageResistance = Math.Round(random.NextDouble(), 2);
+            PhysicalDamageResistance = Math.Round(random.NextDouble(), 2);
+        }
+        #endregion
+    }
+}
